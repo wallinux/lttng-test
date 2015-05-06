871b4e74dfd18e2dc8847db59ad0572da1aade7b
Anders Wallin <anders.wallin@windriver.com>
2015-05-06 12:45:58 +0000

    added testcases

---
+++ test/ust-test/tracetest2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#include "tracetest-tp.h"
+
+#define NUM_THREADS 100
+int maxloop = 1000;
+
+int gettid()  {
+	return (int) syscall(SYS_gettid);
+}
+
+void * thread_function(void * na)
+{
+	int i;
+	static int tid;
+
+	tid = gettid();
+
+	printf("Running thread: %i (%i)\n", tid, maxloop);
+
+	for (i = 0; i < maxloop; i++) {
+		tracepoint(tracetest, first_tp, i, "test");
+	}
+
+	return (void*) NULL;
+}
+
+int main(int argc, char* argv[])
+{
+	pthread_t threads[NUM_THREADS];
+	int rc, t;
+	int *res;
+	int maxthreads = 2;
+
+	printf("usage: %s <no_of_loops> <no_of_threads>\n", argv[0]);
+	if (argc >= 2) {
+		maxloop = atoi(argv[1]);
+	}
+
+	if (argc >= 3) {
+		maxthreads = atoi(argv[2]);
+		if (maxthreads > NUM_THREADS) maxthreads = NUM_THREADS;
+	}
+
+	for (t = 0; t < maxthreads; t++) {
+		rc = pthread_create(&threads[t], NULL, thread_function, (void *) NULL);
+		if (rc) {
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
+		}
+	}
+	for (t = 0; t < maxthreads; t++) {
+		rc = pthread_join(threads[t], (void*) &res);
+		if (rc) {
+			printf("ERROR; return code from pthread_join() is %d\n", rc);
+			exit(-1);
+		}
+		/* printf("join: %i\n", t); */
+	}
+	return 0;
+}
